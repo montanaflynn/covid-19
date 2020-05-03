5ac0da93633a07100e8f8cf0d6b5a7eea65734ee
Montana Flynn <montana949@gmail.com>
2020-05-03 15:01:00 +0700

    Add buttons for changing data, scale and percentage of population options

--- assets/chart.js
+++ assets/chart.js
@@ -6,19 +6,54 @@
 //   yScale: 'linear',
 //   width: 800,
 //   height: 600
-// };
+// }
 function makeChart(opts) {  
-    var selectedData = opts.selectedData || "confirmed";
-    var percentageOfPopulation = opts.percentageOfPopulation || false;
-    var yScale = opts.yScale || 'linear';
-    var width = opts.width || 1200;
-    var height = opts.height || 600;
+    var selectedData = opts.selectedData || "confirmed"
+    var percentageOfPopulation = opts.percentageOfPopulation || false
+    var width = opts.width || window.innerWidth-300 || 1200
+    var height = opts.height || window.innerHeight-300 || 600
+    var originalData = opts.data
+    var chartName = opts.name || "Confirmed Cases"
+    var yScale = getScaleStyle(opts.yScale)
 
     // Set the dimensions of the canvas / graph
-    var margin = {top: 60, right: 300, bottom: 60, left: 60},
+    var margin = {top: 10, right: 300, bottom: 60, left: 60},
     width = width - margin.left - margin.right,
-    height = height - margin.top - margin.bottom;
+    height = height - margin.top - margin.bottom
 
+    function getChartName() {
+        switch(selectedData) {
+            case "recovered":
+                chartName = "Recovered Cases"
+                break;
+            case "recovered":
+                chartName = "Recovered Cases"
+                break;
+            case "deaths":
+                chartName = "Deaths"
+                break;
+            case "active":
+                chartName = "Active Cases"
+                break;
+            default:
+                chartName = "Confirmed Cases"
+            }
+        return percentageOfPopulation ? chartName + " as Percentage of Population" : chartName 
+    }
+
+    function getScaleStyle(scaleString) {
+        console.log(scaleString)
+        switch(scaleString) {
+            case "sqrt":
+                return d3.scaleSqrt()
+            case "pow":
+                return d3.scalePow()
+            case "log":
+                return d3.scaleSymlog()
+            default:
+                return d3.scaleLinear()
+            }
+    }
 
     function getPercentage(d, selectedData) {
         return (d[selectedData]/d.population)*100
@@ -29,18 +64,6 @@ function makeChart(opts) {
     }
 
     function chooseData(d, selectedData) {
-
-        if (selectedData === "active") {
-            console.log(d.label_en, d.confirmed, d.recovered, d.deaths)
-            var activeCases = d.confirmed - d.recovered - d.deaths
-            console.log(activeCases)
-            if (percentageOfPopulation){
-                return (activeCases/d.population)*100
-            } else {
-                return activeCases
-            }
-        }
-
         if (percentageOfPopulation){
             return getPercentage(d, selectedData)
         } else {
@@ -48,78 +71,81 @@ function makeChart(opts) {
         }
     }
 
-    // Parse the date / time
-    var parseDate = d3.timeParse("%Y%m%d");
-
-    // Set the ranges
-    var x = d3.scaleTime().range([0, width]);  
-    var y = d3.scaleSqrt().range([height, 0]);
-    var y = d3.scaleLinear().range([height, 0]);
-
-    // Define the line
-    var line = d3.line()	
-        .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(chooseData(d, selectedData)); });
-
-    var container = d3.select("body")
-        .append("div")
-        .attr("class", "container")
-        .attr("id", opts.id);
-
-    container.append("h2").text(opts.name)
-
-    // Adds the svg canvas
-    var svg = container
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    // Parser for the date / time
+    var parseDate = d3.timeParse("%Y%m%d")
 
     function render(data){
 
+        d3.selectAll(".container").remove()
+
+        var data = JSON.parse(JSON.stringify(data))
+
+        var container = d3.select("body")
+        .append("div")
+        .attr("class", "container")
+
+        container.append("h2").text(getChartName())
+
+        // Adds the svg canvas
+        var svg = container
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
         data = data.filter(function(row) {
-            return row['label_parent_en'] === 'null';
-        });
+            return row['label_parent_en'] === 'null'
+        })
 
         data.forEach(function(d) {
-            d.date = parseDate(d.date);
-            d.val = +chooseData(d, selectedData);
-        });
+            d.date = parseDate(d.date)
+            d.val = +chooseData(d, selectedData)
+        })
 
         var max = function(data){
-            return d3.max(data, function(d) { return d.val; })
+            return d3.max(data, function(d) { return d.val })
         }
 
+        // Set the ranges
+        var x = d3.scaleTime().range([0, width])  
+        var y = yScale.range([height, 0])
+
+        // Define the line
+        var line = d3.line()	
+        .x(function(d) { return x(d.date) })
+        .y(function(d) { return y(chooseData(d, selectedData)) })
+        
+
         // Scale the range of the data
-        x.domain(d3.extent(data, function(d) { return d.date; }));
-        y.domain([0, max(data)]);
+        x.domain(d3.extent(data, function(d) { return d.date }))
+        y.domain([0, max(data)])
 
         // set the colour scale
-        var color = d3.scaleOrdinal(d3.schemeCategory10);
+        var color = d3.scaleOrdinal(d3.schemeCategory10)
 
         // Nest the entries by country
         var dataNest = d3.nest()
-            .key(function(d) {return d.label_en;})
-            .entries(data);
+            .key(function(d) {return d.label_en})
+            .entries(data)
 
         dataNest.sort(function (a, b){
-            if (max(a.values) > max(b.values)) {return -1;} 
-            else if (max(a.values) < max(b.values)) { return 1;} 
-            else return 0;
+            if (max(a.values) > max(b.values)) {return -1} 
+            else if (max(a.values) < max(b.values)) { return 1} 
+            else return 0
         })
 
-        dataNest = dataNest.slice(0, 20);
+        dataNest = dataNest.slice(0, 20)
 
-        legendSpace = height/dataNest.length;
+        legendSpace = height/dataNest.length
 
         // Loop through each symbol / key
         dataNest.forEach(function(d,i) { 
             svg.append("path")
                 .attr("class", "line")
                 .style("stroke", function() {
-                    return d.color = color(d.key); })
-                .attr("d", line(d.values));
+                    return d.color = color(d.key) })
+                .attr("d", line(d.values))
 
             // Add the Legend
             svg.append("text")
@@ -127,101 +153,67 @@ function makeChart(opts) {
                 .attr("x", width + 20)
                 .attr("class", "legend")
                 .style("fill", function() {
-                    return d.color = color(d.key); })
-                .text(d.key); 
+                    return d.color = color(d.key) })
+                .text(d.key) 
+        })
 
-        });
+        // Add the X Axis
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x))
 
-    // Add the X Axis
-    svg.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
+        // Add the Y Axis
+        var axisLeft = d3.axisLeft(y)
+        if (percentageOfPopulation) {
+            axisLeft = d3.axisLeft(y).tickFormat(d => d + "%")
+        } 
 
-    // Add the Y Axis
-    var axisLeft = d3.axisLeft(y)
-    if (percentageOfPopulation) {
-        axisLeft = d3.axisLeft(y).tickFormat(d => d + "%")
-    } 
-
-    svg.append("g")
-        .attr("class", "axis")
-        .call(axisLeft);
+        svg.append("g")
+            .attr("class", "axis")
+            .call(axisLeft.tickArguments([5]))
+    
     }
 
-    // Get the data
-    d3.csv(opts.data).then(function(data) {
-        render(data)
-    });
+    const checkbox = d3.select("#by_population")
+    checkbox.on("change", function(){
+        percentageOfPopulation = d3.select(this).property("checked")
+        render(originalData)
+    })
+
+    const metricRadioButtons = d3.selectAll('input[name="metric"]')
+    metricRadioButtons.on("change", function(){
+        selectedData = this.value
+        render(originalData)
+    })
+
+    const scaleRadioButtons = d3.selectAll('input[name="scale"]')
+    scaleRadioButtons.on("change", function(){
+        yScale = getScaleStyle(this.value)
+        render(originalData)
+    })
+
+    render(originalData)
 }
   
-function getHistoricalData() {
+function getHistoricalDataURL() {
     if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
-        return "data/historical.csv";
+        return "data/historical.csv"
     }
-    return "https://montanaflynn.github.io/covid-19/data/historical.csv";
+    return "https://montanaflynn.github.io/covid-19/data/historical.csv"
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    historicalData = getHistoricalData()
-    makeChart({
-        name: "Confirmed Cases",
-        id: "confirmed",
-        selectedData: "confirmed",
-        data: historicalData
-    });
 
-    makeChart({
-        name: "Confirmed Cases By Population",
-        id: "confirmed_by_population",
-        selectedData: "confirmed",
-        percentageOfPopulation: true,
-        data: historicalData
-    });
+    // get the data
+    d3.csv(getHistoricalDataURL()).then(function(data) {
 
-    makeChart({
-        name: "Deaths Cases",
-        id: "deaths",
-        selectedData: "deaths",
-        data: historicalData
-    });
-
-    makeChart({
-        name: "Deaths Cases By Population",
-        id: "deaths_by_population",
-        selectedData: "deaths",
-        percentageOfPopulation: true,
-        data: historicalData
-    });
-
-    makeChart({
-        name: "Recovered Cases",
-        id: "recovered",
-        selectedData: "recovered",
-        data: historicalData
-    });
-
-    makeChart({
-        name: "Recovered Cases By Population",
-        id: "recovered_by_population",
-        selectedData: "active",
-        percentageOfPopulation: true,
-        data: historicalData
-    });
-
-    makeChart({
-        name: "Active Cases",
-        id: "active",
-        selectedData: "active",
-        data: historicalData
-    });
-
-    makeChart({
-        name: "Active Cases By Population",
-        id: "active_by_population",
-        selectedData: "active",
-        percentageOfPopulation: true,
-        data: historicalData
-    });
-  });
+        // add active cases property
+        data.map(d => d.active = d.confirmed - d.recovered - d.deaths)
+        makeChart({
+            selectedData: "confirmed",
+            data: data
+        })
+    })
+  })
   
